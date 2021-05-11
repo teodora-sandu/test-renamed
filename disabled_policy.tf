e75975e122ea4e4eb12459b02a4c4807a4897fbd
teodora-sandu <81559517+teodora-sandu@users.noreply.github.com>
2021-05-11 15:17:12 +0100

    Update disabled_policy.tf

--- disabled_policy.tf
+++ disabled_policy.tf
@@ -1,7 +1,7 @@
 data "azurerm_storage_account_sas" "allowed" {
   connection_string = "Microsoft.Azure.Commands.Sql.SecureConnection.Model.ConnectionStrings"
-  start             = "2018-03-21"
-  expiry            = "2020-03-21"
+  https_only        = true
+  signed_version    = "2017-07-29"
 
   resource_types {
     service   = true
@@ -16,8 +16,6 @@ data "azurerm_storage_account_sas" "allowed" {
     file  = false
   }
 
-  https_only = true
-
   permissions {
     read    = true
     write   = true
@@ -28,13 +26,14 @@ data "azurerm_storage_account_sas" "allowed" {
     update  = false
     process = false
   }
+  start  = "2020-01-01T00:00:00Z"
+  expiry = "2020-01-01T01:00:00Z"
 }
 
 data "azurerm_storage_account_sas" "allowed_2" {
   connection_string = "Microsoft.Azure.Commands.Sql.SecureConnection.Model.ConnectionStrings"
-
-  start  = "2018-03-21"
-  expiry = "2020-03-21"
+  https_only        = true
+  signed_version    = "2017-07-29"
 
   resource_types {
     service   = true
@@ -48,6 +47,7 @@ data "azurerm_storage_account_sas" "allowed_2" {
     table = false
     file  = false
   }
+
   permissions {
     read    = true
     write   = true
@@ -58,13 +58,110 @@ data "azurerm_storage_account_sas" "allowed_2" {
     update  = false
     process = false
   }
+  start  = "2020-01-01T00:00:00Z"
+  expiry = "2020-01-01T00:30:00Z"
+}
+
+data "azurerm_storage_account_sas" "allowed_3" {
+  connection_string = "Microsoft.Azure.Commands.Sql.SecureConnection.Model.ConnectionStrings"
+  https_only        = true
+  signed_version    = "2017-07-29"
+
+  resource_types {
+    service   = true
+    container = false
+    object    = false
+  }
+
+  services {
+    blob  = true
+    queue = false
+    table = false
+    file  = false
+  }
+
+  permissions {
+    read    = true
+    write   = true
+    delete  = false
+    list    = false
+    add     = true
+    create  = true
+    update  = false
+    process = false
+  }
+  start  = "2020-01-01T00:00:00Z"
+  expiry = "2020-01-01T00:59:59Z"
+}
+
+data "azurerm_storage_account_sas" "allowed_4" {
+  connection_string = "Microsoft.Azure.Commands.Sql.SecureConnection.Model.ConnectionStrings"
+  https_only        = true
+  signed_version    = "2017-07-29"
+
+  resource_types {
+    service   = true
+    container = false
+    object    = false
+  }
+
+  services {
+    blob  = true
+    queue = false
+    table = false
+    file  = false
+  }
+
+  permissions {
+    read    = true
+    write   = true
+    delete  = false
+    list    = false
+    add     = true
+    create  = true
+    update  = false
+    process = false
+  }
+  start  = "2020-01-01T00:00:00"
+  expiry = "2020-01-01T01:00:00"
+}
+
+data "azurerm_storage_account_sas" "allowed_5" {
+  connection_string = "Microsoft.Azure.Commands.Sql.SecureConnection.Model.ConnectionStrings"
+  https_only        = true
+  signed_version    = "2017-07-29"
+
+  resource_types {
+    service   = true
+    container = false
+    object    = false
+  }
+
+  services {
+    blob  = true
+    queue = false
+    table = false
+    file  = false
+  }
+
+  permissions {
+    read    = true
+    write   = true
+    delete  = false
+    list    = false
+    add     = true
+    create  = true
+    update  = false
+    process = false
+  }
+  start  = "20200101T000000"
+  expiry = "20200101T010000"
 }
 
 data "azurerm_storage_account_sas" "denied" {
   connection_string = "Microsoft.Azure.Commands.Sql.SecureConnection.Model.ConnectionStrings"
-  start             = "2018-03-21"
-  expiry            = "2020-03-21"
-  https_only        = false
+  https_only        = true
+  signed_version    = "2017-07-29"
 
   resource_types {
     service   = true
@@ -78,6 +175,7 @@ data "azurerm_storage_account_sas" "denied" {
     table = false
     file  = false
   }
+
   permissions {
     read    = true
     write   = true
@@ -88,4 +186,166 @@ data "azurerm_storage_account_sas" "denied" {
     update  = false
     process = false
   }
+  start  = "2020-01-01"
+  expiry = "2020-01-02"
+}
+
+data "azurerm_storage_account_sas" "denied_2" {
+  connection_string = "Microsoft.Azure.Commands.Sql.SecureConnection.Model.ConnectionStrings"
+  https_only        = true
+  signed_version    = "2017-07-29"
+
+  resource_types {
+    service   = true
+    container = false
+    object    = false
+  }
+
+  services {
+    blob  = true
+    queue = false
+    table = false
+    file  = false
+  }
+
+  permissions {
+    read    = true
+    write   = true
+    delete  = false
+    list    = false
+    add     = true
+    create  = true
+    update  = false
+    process = false
+  }
+  start  = "2020-01-01T00:00:00Z"
+  expiry = "2020-01-01T01:00:01Z"
+}
+
+data "azurerm_storage_account_sas" "denied_3" {
+  connection_string = "Microsoft.Azure.Commands.Sql.SecureConnection.Model.ConnectionStrings"
+  https_only        = true
+  signed_version    = "2017-07-29"
+
+  resource_types {
+    service   = true
+    container = false
+    object    = false
+  }
+
+  services {
+    blob  = true
+    queue = false
+    table = false
+    file  = false
+  }
+
+  permissions {
+    read    = true
+    write   = true
+    delete  = false
+    list    = false
+    add     = true
+    create  = true
+    update  = false
+    process = false
+  }
+  start  = "2020-01-01T00:00:00Z"
+  expiry = "2020-01-01T01:30:01Z"
+}
+
+data "azurerm_storage_account_sas" "denied_4" {
+  connection_string = "Microsoft.Azure.Commands.Sql.SecureConnection.Model.ConnectionStrings"
+  https_only        = true
+  signed_version    = "2017-07-29"
+
+  resource_types {
+    service   = true
+    container = false
+    object    = false
+  }
+
+  services {
+    blob  = true
+    queue = false
+    table = false
+    file  = false
+  }
+
+  permissions {
+    read    = true
+    write   = true
+    delete  = false
+    list    = false
+    add     = true
+    create  = true
+    update  = false
+    process = false
+  }
+  start  = "2020-01-01T00:00:00Z"
+  expiry = "2020-01-01T01:30:01Z"
+}
+
+data "azurerm_storage_account_sas" "denied_5" {
+  connection_string = "Microsoft.Azure.Commands.Sql.SecureConnection.Model.ConnectionStrings"
+  https_only        = true
+  signed_version    = "2017-07-29"
+
+  resource_types {
+    service   = true
+    container = false
+    object    = false
+  }
+
+  services {
+    blob  = true
+    queue = false
+    table = false
+    file  = false
+  }
+
+  permissions {
+    read    = true
+    write   = true
+    delete  = false
+    list    = false
+    add     = true
+    create  = true
+    update  = false
+    process = false
+  }
+  start  = "2020-01-01T00:00:00"
+  expiry = "2020-01-01T01:00:01"
+}
+
+data "azurerm_storage_account_sas" "denied_6" {
+  connection_string = "Microsoft.Azure.Commands.Sql.SecureConnection.Model.ConnectionStrings"
+  https_only        = true
+  signed_version    = "2017-07-29"
+
+  resource_types {
+    service   = true
+    container = false
+    object    = false
+  }
+
+  services {
+    blob  = true
+    queue = false
+    table = false
+    file  = false
+  }
+
+  permissions {
+    read    = true
+    write   = true
+    delete  = false
+    list    = false
+    add     = true
+    create  = true
+    update  = false
+    process = false
+  }
+  start  = "20200101T000000"
+  expiry = "20200101T010001"
 }
