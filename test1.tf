f2130497bf4363cd3e67e7550f03dec12182526a
Teodora Sandu <81559517+teodora-sandu@users.noreply.github.com>
2021-06-07 14:24:36 +0100

    Update test1.tf

--- test1.tf
+++ test1.tf
@@ -1,5 +1,5 @@
 resource "azurerm_app_service" "allowed" {
-  name                = "example-app-service"
+  name                = "example-app-servic"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   app_service_plan_id = "id-xxx"
